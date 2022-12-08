8b76c2c5849b0a1fa1243a162a5228cf6799d43e
Archie <archieshirjaev@gmail.com>
2022-12-08 11:20:48 +0200

    added 6

--- quests/8th dec - multiple quests.py
+++ quests/8th dec - multiple quests.py
@@ -1,4 +1,5 @@
 from math import *
+from random import *
 
 #1
 print("Puu läbimõõdu arvutamine")
@@ -14,6 +15,11 @@ d=sqrt(N**2+M**2)
 print(f"Maatüki diagonaal on {d} m**2")
 
 #3
+aeg = float(input("Mitu tundi kulus sõiduks? "))
+teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+kiirus = aeg/teepikkus
+print()
+print("Sinu kiirus oli " + str(kiirus) + " km/h")
 
 #4
 A1=int(input("sisesta 1. arv => "))
@@ -21,8 +27,21 @@ A2=int(input("sisesta 2. arv => "))
 A3=int(input("sisesta 3. arv => "))
 A4=int(input("sisesta 4. arv => "))
 A5=int(input("sisesta 5. arv => "))
+Keskmine=(A1+A2+A3+A4+A5)/5
+print(f"Keskmine on {Keskmine}")
 
+#5
+print("    @..@")
+print("   (----)")
+print("  ( \__/ )")
+print("  ^^ "'""'" ^^ ")
 
-
+#6 
+a=randint(1,100)
+b=randint(1,100)
+c=randint(1,100)
+print(f"Külg a={a}\nKülg b={b}\nKülg c={c}")
+print(f"Kolmnurga ümbermõõt = {a+b+c}")
+print()
 
 #--------------------------------
