13e60c75760d8435921792aa42a593398219e224
a <a>
2022-12-14 15:15:34 +0200

    фыфыф

--- quests/14th dec.py
+++ quests/14th dec.py
@@ -1,9 +1,30 @@
-﻿from math import *
+﻿
+from math import *
 from random import *
 
-a=6
-b=4
-a=2*a+3*b16
-b=a/2*b
-print(a)
+#1
+num = int(input("Enter a number: "))
+if num >= 0:
+    if num % 2 == 0:
+        print("Чётное")
+else:
+    print("Нечётное")
+
+#2
+num1 = int(input("Первый номер: "))
+num2 = int(input("Второй номер: "))
+num3 = int(input("Третий номер: "))
+if num1 > 0 and num2 > 0 and num3 > 0:
+    if num1 + num2 + num3 == 180:
+        # Check if the triangle is equilateral, isosceles, or scalene
+        if num1 == num2 and num2 == num3:
+            print("Цифры обозначают углы равностороннего треугольника.")
+        elif num1 == num2 or num1 == num3 or num2 == num3:
+            print("Цифры обозначают углы равнобедренного треугольника.")
+        else:
+            print("Цифры обозначают углы разностороннего треугольника.")
+    else:
+        print("Не углы!")
+else:
+    print("Ошибка!")
 
