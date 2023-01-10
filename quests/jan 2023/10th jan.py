8a2013f4a9ce86c187013a7dd5640854f0ab8d04
Lunairayz <archieshirjaev@gmail.com>
2023-01-10 09:56:32 +0200

    rew

--- quests/jan 2023/10th jan.py
+++ quests/jan 2023/10th jan.py
@@ -1,9 +1,32 @@
-﻿#15
+﻿import random
+
+#15
 katsed = 0
 while True:
-    answer = input("Osta elevant ära! ")
+    answer = input("Osta elevant ära, kirjuta 'elevant' et ostma... ")
     katsed += 1
     if answer.lower() == "elevant":
-        print(f"Teil elevandi ostmiseks kulus {katsed}.")
+        print(f"Teil elevandi ostmiseks kulus {katsed} katse.")
         break
+
+#16
+while True:
+    print("Mõtlesin numbrile 1-10 vahel. Püüa ära arvata, mis see on!")
+    arvutinumber = random.randint(1, 10)
+    while True:
+        proov = input("Arvake ära number, mis ma mõtlesin! (Või tippige 'end', et lõpetada): ")
+        if proov.lower() == "end":
+            exit()
+        try:
+            proov = int(proov)
+        except ValueError:
+            print("Mida? Anda number palun.")
+            continue
+        if proov < arvutinumber:
+            print("liiga madalale!")
+        elif proov > arvutinumber:
+            print("Liiga kõrgel!")
+        else:
+            print("Õige! my number oli", arvutinumber)
+            break
 
--- quests/jan 2023/9th jan.py
+++ quests/jan 2023/9th jan.py
@@ -1,26 +1,26 @@
 ﻿#1
-for i in range(2, 12, 3):
-    print(i, end=" ")
-print()
-for i in range(12, 2, -2):
-    print(i, end=" ")
-print()
+for i in range(1,31,2):
+    print(i, end=» «)
 
 #2.1
 print("Arvuta peast! ...4*100-55")
-o_vastus=4*100-55
-vastus=int(input("Lahenda ülesanne..."))
-k=1
-while vastus!=o_vastus:
-    print("Viga! Sisesta Õige vastus...", )
-    vastus=int(input("Sisesta vastus..."))
-    k+=1
-print("Õige vastus! Katsed oli ...",k )
+o_vastus = 4*100-55
+katsed = 0
+while True:
+    vastus = int(input("Lahenda ülesanne... "))
+    katsed += 1
+    if vastus == o_vastus:
+        print(f"Õige vastus! Katsed oli... {katsed}")
+        break
+    else:
+        print("Viga! Sisesta Õige vastus...")
+
 
 #2.2
 x=0
-while x<30:
+while True:
     if x%2==1:
-        print(x, end=" ")
+        print(x, end=» «)
+    if x== 30:
+        break
     x+=1
-
