88716a372ab805e03a5cb2bef8410d20269d289a
Lunairayz <archieshirjaev@gmail.com>
2023-01-10 09:24:14 +0200

    #15

---
+++ quests/jan 2023/10th jan.py
@@ -0,0 +1,9 @@
+﻿#15
+katsed = 0
+while True:
+    answer = input("Osta elevant ära! ")
+    katsed += 1
+    if answer.lower() == "elevant":
+        print(f"Teil elevandi ostmiseks kulus {katsed}.")
+        break
+
