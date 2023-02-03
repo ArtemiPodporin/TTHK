518dbd8eff69ec2d3e425eeba0b9a11463f91bd5
Luna <archieshirjaev@gmail.com>
2023-02-03 14:07:36 +0200

    dfghdfg

--- quests/2023/february/3 feb.py
+++ quests/2023/february/3 feb.py
@@ -1,45 +1,42 @@
-﻿from math import *
-import datetime
+﻿import datetime
+import math
 
 ikoodid = []
 arvud = []
 
-# Counter for number of attempts
+# попытки
 count = 0
 
-while count < 5:
-  personal_code = input("Enter your personal code: ")
+while count < 4:
+  personal_code = input("Введи свой код!: ")
   
-  # Check if the personal code is 11 characters long
+  # проверка длины
   if len(personal_code) != 11:
-    print("Error: Incorrect number of digits. Please try again.")
+    print("Неправльная длина, должно быть 11.")
     arvud.append(personal_code)
     count += 1
     continue
   
-  # Check the first character
+  # Проверить первую цифрку
   if personal_code[0] not in "123456":
-    print("Error: Incorrect first character. Please try again.")
+    print("Неправильная первая цифра, какой твой пол??")
     arvud.append(personal_code)
     count += 1
     continue
   
-  # Extract the date of birth from the personal code
+  # вычислить до с кода
   day = personal_code[5:7]
   month = personal_code[3:5]
   year = personal_code[1:3]
   
-  # Check if the date of birth is valid
+  # проверить день рождения
   try:
     datetime.datetime.strptime(day + "-" + month + "-" + year, '%d-%m-%y')
   except ValueError:
-    print("Error: Incorrect date of birth. Please try again.")
+    print("Ты родился? Неправльное ДР.")
     arvud.append(personal_code)
     count += 1
     continue
-  
-  # Check the control number
-  # ...
   
   # If the control number is correct, add the personal code to the ikoodid list
   ikoodid.append(personal_code)
@@ -49,7 +46,8 @@ while count < 5:
   dob = day + "/" + month + "/19" + year
   place_of_birth = "Idavir Keshaigla (Kohtla-Järve, endine Jõhvi)"
   print(f"This is a {gender}, his/her birthday is {dob} and place of birth is {place_of_birth}.")
-  break
+  
+  count += 1
 
 # Display the arvud list sorted from lowest to highest
 arvud.sort()
