664c8e95e26d88eb8a8169643c18505da3ea07c2
Luna <archieshirjaev@gmail.com>
2023-02-06 12:00:36 +0200

    fgfdhdf

--- quests/2023/february/3 feb.py
+++ quests/2023/february/3 feb.py
@@ -1,60 +1,68 @@
 ﻿import datetime
-import math
 
-ikoodid = []
+ikodid = []
 arvud = []
 
-# попытки
-count = 0
-
-while count < 4:
-  personal_code = input("Введи свой код!: ")
-  
-  # проверка длины
-  if len(personal_code) != 11:
-    print("Неправльная длина, должно быть 11.")
-    arvud.append(personal_code)
-    count += 1
+for i in range(4):
+  KOOD = input("Andke isikukood palun!: ")
+  if len(KOOD) != 11:
+    print("Andke palun 11-length isikukood.")
+    arvud.append(KOOD)
     continue
   
-  # Проверить первую цифрку
-  if personal_code[0] not in "123456":
-    print("Неправильная первая цифра, какой твой пол??")
-    arvud.append(personal_code)
-    count += 1
+  if KOOD[0] not in ["1", "2", "3", "4", "5", "6"]:
+    print("Esimene number peab olema 1-st kuni 6-ni.")
+    arvud.append(KOOD)
     continue
+
+  SUNNIPAEV = datetime.datetime.strptime(KOOD[1:7], '%d%m%y').date()
+
+  # Control number calculation
+  KAAL1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+  KAAL2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+  sum = 0
+  for j in range(10):
+    sum += int(KOOD[j]) * KAAL1[j]
   
-  # вычислить до с кода
-  day = personal_code[5:7]
-  month = personal_code[3:5]
-  year = personal_code[1:3]
-  
-  # проверить день рождения
-  try:
-    datetime.datetime.strptime(day + "-" + month + "-" + year, '%d-%m-%y')
-  except ValueError:
-    print("Ты родился? Неправльное ДР.")
-    arvud.append(personal_code)
-    count += 1
+  KONTROLLNUMBER = sum % 11
+  if KONTROLLNUMBER == 10:
+    sum = 0
+    for j in range(10):
+      sum += int(KOOD[j]) * KAAL2[j]
+    KONTROLLNUMBER = sum % 11
+
+  if KONTROLLNUMBER != int(KOOD[10]):
+    print("Invalid control number. Please enter a valid personal code.")
+    arvud.append(KOOD)
     continue
-  
-  # If the control number is correct, add the personal code to the ikoodid list
-  ikoodid.append(personal_code)
-  
-  # Display the information
-  gender = "male" if personal_code[0] in "135" else "female"
-  dob = day + "/" + month + "/19" + year
-  place_of_birth = "Idavir Keshaigla (Kohtla-Järve, endine Jõhvi)"
-  print(f"This is a {gender}, his/her birthday is {dob} and place of birth is {place_of_birth}.")
-  
-  count += 1
 
-# Display the arvud list sorted from lowest to highest
-arvud.sort()
-print("List of incorrect codes:", arvud)
+  if KOOD[0] in ["1", "3", "5"]:
+    SUGU = "MEES"
+  else:
+    SUGU = "NAINE"
 
-# Display the ikoodid list, first for women and then for men
-ikoodid_women = [code for code in ikoodid if code[0] in "246"]
-ikoodid_men = [code for code in ikoodid if code[0] in "135"]
-print("List of correct codes for women:", ikoodid_women)
-print("List of correct codes for men:", ikoodid_men)
+  HAIGLA = ""
+  if KOOD[7:10] == "001":
+    HAIGLA = "Kuressaare Hospital"
+  elif KOOD[7:10] == "011":
+    HAIGLA = "Tartu University Women's Clinic, Tartu County, Tartu"
+  elif KOOD[7:10] >= "021" and KOOD[7:10] <= "220":
+    HAIGLA = "Ida-Tallinn Central Hospital, Pelgulinna Maternity Hospital, Hiiumaa, Keila, Rapla Hospital, Loksa Hospital"
+  elif KOOD[7:10] >= "221" and KOOD[7:10] <= "270":
+    HAIGLA = "Ida-Viru Central Hospital (Kohtla-Järve)"
+  elif KOOD[7:10] >= "271" and KOOD[7:10] <= "370":
+    HAIGLA = "Tartu clinic, Riverva Hospital"
+  elif KOOD[7:10] >= "371" and KOOD[7:10] <= "420":
+    HAIGLA = "Narva Hospital"
+  elif KOOD[7:10] >= "421" and KOOD[7:10] <= "470":
+    HAIGLA = "Pärnu Hospital"
+  elif KOOD[7:10] >= "471" and KOOD[7:10] <= "490":
+    HAIGLA = "Tallinn Pelgulinna Maternity Hospital, Haapsalu Hospital"
+  elif KOOD[7:10] >= "491" and KOOD[7:10] <= "520":
+    HAIGLA = "Järvamaa Hospital (Paide)"
+  elif KOOD[7:10] >= "521" and KOOD[7:10] <= "570":
+    HAIGLA = "Rakvere, Tapa Hospital"
+  elif KOOD[7:10] >= "571" and KOOD[7:10] <= "600":
+    HAIGLA = "Valga Hospital"
+  elif KOOD[7:10] >= "601" and KOOD[7:10] <= "650":
+    HAIGLA = "Valga Hospital"
