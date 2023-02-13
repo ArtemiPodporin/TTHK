c9411a2c30815613114d0b50deef7882820b04d8
Lunairayz <archieshirjaev@gmail.com>
2023-02-13 11:22:56 +0200

    икууу

--- quests/2023/february/feb 10.py
+++ quests/2023/february/feb 10.py
@@ -97,3 +97,22 @@ def date(день, месяц, год):
         if день not in range(1, 29):
             return False
     return True
+
+#8
+def XOR_cipher(inputstring, ключ):
+    """
+    Функция XOR_cipher, которая принимает 2 аргумента: строку для шифрования и ключ шифрования,
+    и возвращает строку, зашифрованную путем применения функции XOR (^) к символам строки с ключом.
+    """
+    inputstring = str(inputstring)
+    ключ = str(ключ)
+    input_string_length = len(inputstring)
+    длинаКлюча = len(ключ)
+    индексключа = 0
+    encryptedstring = ""
+    for буква in inputstring:
+        if индексключа >= длинаКлюча:
+            индексключа = 0
+        encryptedstring += chr(ord(буква) ^ ord(ключ[индексключа]))
+        индексключа += 1
+    return encryptedstring
--- quests/2023/february/feb 13.py
+++
@@ -1,2 +0,0 @@
-﻿import math
-import random 
