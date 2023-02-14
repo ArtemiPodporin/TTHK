f1ea7b80a07192665dded36c5c1078fb9dc72259
Lunairayz <archieshirjaev@gmail.com>
2023-02-14 12:40:20 +0200

    а

--- quests/2023/february/feb 14 - autoriseerimine/autoriseerimine.py
+++ quests/2023/february/feb 14 - autoriseerimine/autoriseerimine.py
@@ -1,1 +1,15 @@
-﻿
+﻿from MyModule import register, login, logout
+
+login_list = []
+password_list = []
+
+while True:
+    action = input("Enter '1' to register, '2' to log in, or '3' to log out: ")
+    if action == '1':
+        register(login_list, password_list)
+    elif action == '2':
+        login(login_list, password_list)
+    elif action == '3':
+        logout()
+    else:
+        print("Invalid action.")
