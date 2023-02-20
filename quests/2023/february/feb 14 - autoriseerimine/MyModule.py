86172fc03aee537c51163240b177323ba02a848c
Lunairayz <archieshirjaev@gmail.com>
2023-02-20 12:13:07 +0200

    yes

--- quests/2023/february/feb 14 - autoriseerimine/MyModule.py
+++ quests/2023/february/feb 14 - autoriseerimine/MyModule.py
@@ -1,36 +1,94 @@
 ﻿import string
 from random import choice
 
-def random_salasona(k: int):
-    sala=""
-    for i in range(k):
-        t=choice(string.ascii_letters) #Aa...Zz
-        num=choice([1,2,3,4,5,6,7,8,9,0])
-        t_num=[t,str(num)]
-        sala+=choice(t_num)         
-    return sala
+# initialize empty lists for logins and passwords
+logins = []
+passwords = []
 
-def register(list_of_logins, list_of_passwords):
-    login = input("Выбери себе логин: ")
-    while login in list_of_logins:                                                                             
-        login = input("Такой логин уже есть! Выбери новый: ")
-    password_type = input("Набери '1' чтобы сгенерировать, или '2' чтобы написать самому: ")
-    if password_type == '1':
-        password = random_salasona(8)
-        print("Ваш рандом пароль:", password)
+# function for generating a random password
+def salasona(k: int):
+  sala=""
+  for i in range(k):
+   t=choice(string.ascii_letters) #Aa...Zz
+   num=choice([1,2,3,4,5,6,7,8,9,0])
+   t_num=[t,str(num)]
+   sala+=choice(t_num)
+  return sala
+
+# function for user registration
+def register():
+    login = input("Enter your name: ")
+    if login in logins:
+        print("This name is already taken.")
+        return
+    password_choice = input("Would you like to generate a password? (Y/N): ")
+    if password_choice.lower() == 'y':
+        password = salasona(8)
+        print(f"Your password is: {password}")
     else:
-        password = input("Выбери себе пароль: ")
-    list_of_logins.append(login)
-    list_of_passwords.append(password)
-    print("Вы зарегистрировались!")
+        while True:
+            password = input("Enter your password: ")
+            if any(char.isdigit() for char in password) and any(char.islower() for char in password) and any(char.isupper() for char in password) and any(char in string.punctuation for char in password):
+                break
+            else:
+                print("Your password must contain at least one number, one lowercase character, one uppercase character, and one special symbol.")
+    logins.append(login)
+    passwords.append(password)
+    print("Registration complete.")
 
-def login(list_of_logins, list_of_passwords):
-    login = input("Вход: Введи свой логин: ")
-    password = input("Вход: Ввежи свой пароль: ")
-    if login in list_of_logins and password == list_of_passwords[list_of_logins.index(login)]:
-        print("Вы успешно вошли!")
+# function for user authorization
+def authorize():
+    login = input("Enter your name: ")
+    if login not in logins:
+        print("This name is not registered.")
+        return
+    password = input("Enter your password: ")
+    if password != passwords[logins.index(login)]:
+        print("Incorrect password.")
+        return
+    print("Authorization successful.")
+
+# function for changing name or password
+def change():
+    login = input("Enter your name: ")
+    if login not in logins:
+        print("This name is not registered.")
+        return
+    password = input("Enter your password: ")
+    if password != passwords[logins.index(login)]:
+        print("Incorrect password.")
+        return
+    choice = input("Would you like to change your name or password? (login/password): ")
+    if choice.lower() == 'login':
+        new_login = input("Enter your new name: ")
+        if new_login in logins:
+            print("This name is already taken.")
+            return
+        logins[logins.index(login)] = new_login
+        print("Name change successful.")
+    elif choice.lower() == 'password':
+        while True:
+            new_password = input("Enter your new password: ")
+            if any(char.isdigit() for char in new_password) and any(char.islower() for char in new_password) and any(char.isupper() for char in new_password) and any(char in string.punctuation for char in new_password):
+                break
+            else:
+                print("Your password must contain at least one number, one lowercase character, one uppercase character, and one special symbol.")
+        passwords[logins.index(login)] = new_password
+        print("Password change successful.")
     else:
-        print("Что-то неправильно!!")
+        print("Invalid choice.")
 
+# function for restoring forgotten password
+def forgotpassword():
+    login = input("Наберите свой логин: ")
+    if login not in logins:
+        print("Вы не зарегистрированы.")
+        return
+    new_password = salasona(8)
+    passwords[logins.index(login)] = new_password
+    print(f"Your new password is: {new_password}")
+
+# function for logging out
 def logout():
     print("Вы вышли.")
+    
--- quests/2023/february/feb 14 - autoriseerimine/autoriseerimine.py
+++ quests/2023/february/feb 14 - autoriseerimine/autoriseerimine.py
@@ -1,14 +1,14 @@
 ﻿from MyModule import register, login, logout
 
-list_of_logins = []
-list_of_passwords = []
+logins = []
+passwords = []
 
 while True:
     действие = input("Нажми: '1' чтобы зарегистрироватся, '2' чтобы войти, или '3' чтобы выйти: ")
     if действие == '1':
-        register(list_of_logins, list_of_passwords)
+        register(logins, passwords)
     elif действие == '2':
-        login(list_of_logins, list_of_passwords)
+        login(logins, passwords)
     elif действие == '3':
         logout()
     else:
