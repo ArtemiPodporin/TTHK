d666398d7a08b4ecc54609cdef86ffd7d89de751
Lunairayz <archieshirjaev@gmail.com>
2023-02-21 14:04:23 +0200

    a

--- quests/2023/february/feb 21 - palgad/MyModule.py
+++
@@ -1,9 +0,0 @@
-﻿palk = [1200,2500,750,395,1200]
-inimesed = ["A","B","C","D","E"]
-
-def leiaPalk(inimene):
-palgad = []
-for i in range(len(inimesed)):
-if inimesed[i] == inimene:
-palgad.append(palk[i])
-return palgad
--- quests/2023/february/feb 21 - palgad/jah.py
+++
@@ -1,9 +0,0 @@
-﻿import MyModule
-
-inimine = input("Sisesta nimi: ")
-palgad = MyModule.leiaPalk(inimine)
-
-if len(palgad) == 0:
-    print("Ei leidsid palga", inimine)
-else:
-    print("Palk", inimine, "on:", palgad)
