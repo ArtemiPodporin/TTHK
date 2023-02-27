5a26974bbb3aa9acf86f4acf132af82cee3a9452
Lunairayz <archieshirjaev@gmail.com>
2023-02-27 12:16:27 +0200

    a

--- quests/2023/february/feb 27.py
+++ quests/2023/february/feb 27.py
@@ -1,10 +1,31 @@
-﻿def Kustutamine(i:list,p:list):
-    nimi=input("sisesta nimi: ")
-    if nimi in i:
-        n=i.count(nimi)
-        for j in range(n):
-            ind=i.index(nimi)
-            i.pop(ind)
-            p.pop(ind)
+﻿def Sorteerimine(i:list,p:list):
+    """Kirjeldus....kirjuta ise!!!
+    :param list i: Inimeste järjend
+    :param list p: Palgade järjend
+    :rtype: list, list
+    """
+    v=int(input("palk 1-kahaneb,2-kasvab?"))
+    if v==1:
+        n=len(p)
+        for j in range(0,n-1):
+            for k in range(j+1,n):
+                if p[j]<p[k]:
+                    abi=p[j]
+                    p[j]=p[k]
+                    p[k]=abi
+                    abi=i[j]
+                    i[j]=i[k]
+                    i[k]=abi
+    elif v==2:
+        n=len(p)
+        for j in range(0,n-1):
+            for k in range(j+1,n):
+                if p[j]>p[k]:
+                    abi=p[j]
+                    p[j]=p[k]
+                    p[k]=abi
+                    abi=i[j]
+                    i[j]=i[k]
+                    i[k]=abi
 
     return i,p
