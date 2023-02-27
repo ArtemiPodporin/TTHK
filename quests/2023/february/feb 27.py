bca9b47fc9f03ed8367f22909873ec958ac5729f
Lunairayz <archieshirjaev@gmail.com>
2023-02-27 11:52:18 +0200

    fgd

---
+++ quests/2023/february/feb 27.py
@@ -0,0 +1,10 @@
+﻿def Kustutamine(i:list,p:list):
+    nimi=input("sisesta nimi: ")
+    if nimi in i:
+        n=i.count(nimi)
+        for j in range(n):
+            ind=i.index(nimi)
+            i.pop(ind)
+            p.pop(ind)
+
+    return i,p
