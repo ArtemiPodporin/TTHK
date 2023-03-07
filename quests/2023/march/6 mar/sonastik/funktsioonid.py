a7957577cfd6a619f1da8e32fe64cafc15460ecb
Lunairayz <archieshirjaev@gmail.com>
2023-03-07 14:22:37 +0200

    sdad

--- quests/2023/march/6 mar/sonastik/funktsioonid.py
+++ quests/2023/march/6 mar/sonastik/funktsioonid.py
@@ -1,56 +1,112 @@
-import rus.txt
-import est.txt
+# Import necessary libraries
+import random
 
-def tolgi_sona(sona, mis_keelest, mis_keele):
-    if mis_keelest == "est" and mis_keele == "rus":
-        allika_loend = est
-        siht_loend = rus
-    elif mis_keelest == "rus" and mis_keele == "est":
-        allika_loend = rus
-        siht_loend = est
-    else:
-        return "Toetamata tolge"
+# Define file paths
+ESTONIAN_DICT_PATH = "est.txt"
+RUSSIAN_DICT_PATH = "rus.txt"
 
-    if sona in allika_loend:
-        index = allika_loend.index(sona)
-        return siht_loend[index]
+# Define translation functions
+def translate(word, from_language, to_language):
+    if from_language == "est" and to_language == "rus":
+        dict_path = ESTONIAN_DICT_PATH
+    elif from_language == "rus" and to_language == "est":
+        dict_path = RUSSIAN_DICT_PATH
     else:
-        return None
+        return "Unsupported languages"
+    
+    with open(dict_path, "r", encoding="utf-8") as dict_file:
+        for line in dict_file:
+            terms = line.strip().split("=")
+            if len(terms) == 2:
+                if from_language == "est":
+                    if word == terms[0]:
+                        return terms[1]
+                elif from_language == "rus":
+                    if word == terms[1]:
+                        return terms[0]
+    return "Word not found in dictionary"
 
-def lisa_sona(sona, keel):
-    if keel == "est":
-        siht_loend = est
-        faili_nime = "est.txt"
-    elif keel == "rus":
-        siht_loend = rus
-        faili_nime = "rus.txt"
+# Define function to add new words to dictionary
+def add_word(from_word, to_word, from_language, to_language):
+    if from_language == "est" and to_language == "rus":
+        dict_path = ESTONIAN_DICT_PATH
+    elif from_language == "rus" and to_language == "est":
+        dict_path = RUSSIAN_DICT_PATH
     else:
-        return "Toetamata tolge"
+        return "Unsupported languages"
+    
+    with open(dict_path, "a", encoding="utf-8") as dict_file:
+        dict_file.write(f"\n{from_word}={to_word}")
+    
+    return "Word added to dictionary"
 
-    if sona not in siht_loend:
-        siht_loend.append(sona)
-        with open(faili_nime, "a", encoding="utf-8") as f:
-            f.write(sona + "\n")
-        return "sona added"
+# Define function to edit existing words in dictionary
+def edit_word(word, new_translation, from_language, to_language):
+    if from_language == "est" and to_language == "rus":
+        dict_path = ESTONIAN_DICT_PATH
+    elif from_language == "rus" and to_language == "est":
+        dict_path = RUSSIAN_DICT_PATH
     else:
-        return "sona juba eksisteerib"
+        return "Unsupported languages"
+    
+    with open(dict_path, "r", encoding="utf-8") as dict_file:
+        lines = dict_file.readlines()
+    
+    found_word = False
+    with open(dict_path, "w", encoding="utf-8") as dict_file:
+        for line in lines:
+            terms = line.strip().split("=")
+            if len(terms) == 2:
+                if from_language == "est":
+                    if word == terms[0]:
+                        dict_file.write(f"{word}={new_translation}\n")
+                        found_word = True
+                    else:
+                        dict_file.write(line)
+                elif from_language == "rus":
+                    if word == terms[1]:
+                        dict_file.write(f"{new_translation}={word}\n")
+                        found_word = True
+                    else:
+                        dict_file.write(line)
+            else:
+                dict_file.write(line)
+    
+    if found_word:
+        return "Word edited in dictionary"
+    else:
+        return "Word not found in dictionary"
 
-def correct_sona(vana_sona, uus_sona, keel):
-    if keel == "est":
-        siht_loend = est
-        faili_nime = "est.txt"
-    elif keel == "rus":
-        siht_loend = rus
-        faili_nime = "rus.txt"
+# Define function to test knowledge of words
+def test_knowledge(num_questions, from_language, to_language):
+    if from_language == "est" and to_language == "rus":
+        dict_path = ESTONIAN_DICT_PATH
+    elif from_language == "rus" and to_language == "est":
+        dict_path = RUSSIAN_DICT_PATH
     else:
-        return "Toetamata tolge"
-
-    if vana_sona in siht_loend:
-        index = siht_loend.index(vana_sona)
-        siht_loend[index] = uus_sona
-        with open(faili_nime, "w", encoding="utf-8") as f:
-            for sona in siht_loend:
-                f.write(sona + "\n")
-        return "sona parandatud"
-    else:
-        return "sona ei leitud"
+        return "Unsupported languages"
+    
+    with open(dict_path, "r", encoding="utf-8") as dict_file:
+        lines = dict_file.readlines()
+    
+    questions = random.sample(lines, min(num_questions, len(lines)))
+    num_correct = 0
+    for question in questions:
+        terms = question.strip().split("=")
+        if len(terms) == 2:
+            if from_language == "est":
+                prompt = f"What is the Russian translation of '{terms[0]}'? "
+                correct_answer = terms[1]
+            elif from_language == "rus":
+                prompt = f"What is the Estonian translation of '{terms[1]}'? "
+                correct_answer = terms[0]
+            
+            user_answer = input(prompt)
+            if user_answer == correct_answer:
+                num_correct += 1
+                print("Correct!")
+            else:
+                print(f"Incorrect. The correct answer is '{correct_answer}'")
+    
+    percent_correct = num_correct / len(questions) * 100
+    return f"{num_correct}/{len(questions)} ({percent_correct:.2f}%) answers were correct"
--- quests/2023/march/6 mar/sonastik/tester.py
+++ quests/2023/march/6 mar/sonastik/tester.py
@@ -1,40 +1,40 @@
-import random
-import est.txt
-import rus.txt
+from funktsioonid import translate, add_word, edit_word, test_knowledge
 
-def test_words(lang):
-    if lang == "est":
-        source_list = est
-        target_list = rus
-    elif lang == "rus":
-        source_list = rus
-        target_list = est
-    else:
-        return "Unsupported language"
-
-    correct = 0
-    total = 0
-
-    while True:
-        word = random.choice(source_list)
-        index = source_list.index(word)
-        expected_translation = target_list[index]
-
-        print(f"What is the translation of '{word}'?")
-        answer = input("> ")
-
-        if answer == expected_translation:
-            print("Correct!")
-            correct += 1
-        else:
-            print(f"Incorrect. The correct translation is '{expected_translation}'.")
-
-        total += 1
-
-        print(f"You have answered {correct} out of {total} correctly.")
-
-        if input("Do you want to continue? (y/n) ").lower() != "y":
+# main.py
+while True:
+        print("\nOptions:")
+        print("1. Translate from Estonian to Russian")
+        print("2. Translate from Russian to Estonian")
+        print("3. Add a new word to dictionary")
+        print("4. Edit an existing word in dictionary")
+        print("5. Test knowledge of words")
+        print("6. Quit")
+        
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            word = input("Enter word to translate: ").strip()
+            result = translate(word, "est", "rus")
+            print(result)
+        elif choice == "2":
+            word = input("Enter word to translate: ").strip()
+            result = translate(word, "rus", "est")
+            print(result)
+        elif choice == "3":
+            from_word = input("Enter word in original language: ").strip()
+            to_word = input("Enter translation: ").strip()
+            result = add_word(from_word, to_word, "est", "rus")
+            print(result)
+        elif choice == "4":
+            word = input("Enter word to edit: ").strip()
+            new_translation = input("Enter new translation: ").strip()
+            result = edit_word(word, new_translation, "est", "rus")
+            print(result)
+        elif choice == "5":
+            num_questions = int(input("Enter number of questions: "))
+            result = test_knowledge(num_questions, "est", "rus")
+            print(result)
+        elif choice == "6":
+            print("Exiting program...")
             break
-
-    print(f"You answered {correct} out of {total} correctly ({correct / total * 100:.2f}%).")
-
+        else:
+            print("Invalid choice. Please try again.")
