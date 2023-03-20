63515438452a4969b39f993c1c73c08d2bc54899
Lunairayz <archieshirjaev@gmail.com>
2023-03-20 12:31:15 +0200

    sdfsdf

--- quests/2023/march/20 mar/yeyas.py
+++ quests/2023/march/20 mar/yeyas.py
@@ -1,25 +1,36 @@
 ﻿import tkinter as tk
+from math import *
 
 def show_bahamas():
-    canvas.create_rectangle(0, 0, 300, 200, fill="#7fffd4")
-    canvas.create_rectangle(0, 66, 300, 133, fill="yellow")
-    canvas.create_polygon([0, 0], [100, 100], [0, 200], fill="black")
+    canvas.create_rectangle(0, 0, 300, 200, fill="blue")
+    canvas.create_polygon([150, 0, 0, 100, 150, 200], fill="yellow")
+    canvas.create_polygon([150, 0, 300, 100, 150, 200], fill="yellow")
 
 def show_estonia():
-    canvas.create_rectangle(0, 0, 300, 200/3.0*1.0 , fill="blue")
-    canvas.create_rectangle(0 ,200/3.0*1.0 ,300 ,200/3.0*2.0 ,fill="black")
-    canvas.create_rectangle(0 ,200/3.0*2.0 ,300 ,200/3.0*3.1 ,fill="white")
+    canvas.create_rectangle(0 ,0 ,300 ,66.6 ,fill ="blue")
+    canvas.create_rectangle(0 ,66.6 ,300 ,133.2 ,fill ="black")
+    canvas.create_rectangle(0 ,133.2 ,300 ,200 ,fill ="white")
+
+def show_japan():
+    canvas.create_oval(100-60/2**0.5+50-10/2**0.5,
+                       (100-60/2**0.5+50-10/2**0.5)*3/4,
+                       (100+60/2**0.5+50+10/2**0.5),
+                       (100+60/2**0.5+50+10/2**0.5)*3/4,
+                       fill='red')
 
 root = tk.Tk()
-root.title("Lippud")
+root.title("Flags")
 
-canvas = tk.Canvas(root,width=300,height=200,bg="cyan")
+canvas = tk.Canvas(root,width=300,height=200,bg='white')
 canvas.pack()
 
-bahamas_button = tk.Button(root,text="Flag of the Bahamas",command=show_bahamas)
-bahamas_button.pack()
+bahamas_button = tk.Button(root,text="Flag of Bahamas",command=show_bahamas)
+bahamas_button.pack(side=tk.LEFT)
 
-estonia_button = tk.Button(root,text="Flag of the Estonian state",command=show_estonia)
-estonia_button.pack()
+estonia_button = tk.Button(root,text="Flag of Estonia",command=show_estonia)
+estonia_button.pack(side=tk.LEFT)
+
+japan_button = tk.Button(root,text="Flag of Japan",command=show_japan)
+japan_button.pack(side=tk.LEFT)
 
 root.mainloop()
